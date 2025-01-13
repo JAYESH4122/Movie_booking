18d075b134b23558955ed586e4207fb18abfebc7
JAYESH PJ <133850406+JAYESH4122@users.noreply.github.com>
2025-01-13 20:54:19 +0530

    Update Reservation.js

--- models/Reservation.js
+++ models/Reservation.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const reservationSchema = new mongoose.Schema({
   movieId: mongoose.Schema.Types.ObjectId,
   seats: [String],
-  user: String, // Name of the person reserving
+  user: { type: String, unique: true } // Name of the person reserving
 });
 
 module.exports = mongoose.model('Reservation', reservationSchema);
