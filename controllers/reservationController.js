3c78fedbfb4feb518e8bd29a8520ece33b3ea292
JAYESH PJ <133850406+JAYESH4122@users.noreply.github.com>
2025-01-13 21:08:22 +0530

    Update reservationController.js

--- controllers/reservationController.js
+++ controllers/reservationController.js
@@ -8,6 +8,14 @@ exports.reserveSeats = async (req, res) => {
   const movieId = req.params.id;
 
   try {
+    // Check if the user already has a reservation
+    const existingReservation = await Reservation.findOne({ user });
+
+    if (existingReservation) {
+      // If a reservation already exists for this user, return an error message
+      return res.status(400).json({ message: 'This user has already reserved seats.' });
+    }
+
     // Save the reservation in the database
     const reservation = new Reservation({
       movieId,
@@ -20,6 +28,7 @@ exports.reserveSeats = async (req, res) => {
     // Redirect to the ticket view with reservation details
     res.redirect(`/ticket/${reservation._id}`);
   } catch (err) {
+    console.error(err);
     res.status(500).json({ message: 'Error reserving seats', error: err });
   }
 };
