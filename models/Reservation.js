25fe1d939383d69803ba9cd9648a40ae82c5d3d6
JAYESH PJ <133850406+JAYESH4122@users.noreply.github.com>
2025-01-14 09:34:48 +0530

    Update Reservation.js

--- models/Reservation.js
+++ models/Reservation.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose');
 
-const reservationSchema = new mongoose.Schema({
-  movieId: mongoose.Schema.Types.ObjectId,
-  seats: [String],
-  user: { type: String, unique: true } // Name of the person reserving
-});
+const reservationSchema = new mongoose.Schema(
+  {
+    user: { type: String, required: true },
+    movieId: { type: mongoose.Schema.Types.ObjectId, ref: 'Movie', required: true },
+    seats: { type: [String], required: true },
+  },
+  { timestamps: true } // Enables createdAt and updatedAt fields
+);
 
 module.exports = mongoose.model('Reservation', reservationSchema);
