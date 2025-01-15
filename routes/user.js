6eb47157df939ffb9c01ae0eb6408bb1986539e7
JAYESH PJ <133850406+JAYESH4122@users.noreply.github.com>
2025-01-15 19:49:35 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -7,12 +7,12 @@ const reservationController = require('../controllers/reservationController');
 router.get('/', movieController.getAllMovies);
 
 // Get specific movie and show seat reservation form
-router.get('/reserve/:id', movieController.getMovieById);
+router.get('/reserv/:id', movieController.getMovieById);
 
 // Reserve seats for a movie
-router.post('/reserve/:id', reservationController.reserveSeats);
+router.post('/reserv/:id', reservationController.reserveSeats);
 
 // Route to show the ticket
-router.get('/ticket/:id', reservationController.getTicket);
+router.get('/ticke/:id', reservationController.getTicket);
 
 module.exports = router;
